e81ea47623861e301f87c5feb210013024700d0c
sahrabullah7 <145543132+sahrabullah7@users.noreply.github.com>
2024-12-08 10:42:31 +0200

    Update sever.js
    register

--- sever.js
+++ sever.js
@@ -4,3 +4,24 @@ const db_access = require('./db.js')
 const db = db_access.db
 const port = 127
 server.use(express.json()) //instead of using body parser
+
+//REGISTERATION
+server.post(`/user/register`, (req, res) => 
+{ //req body
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+    const user_role = req.body.user_role
+    const content = `INSERT INTO USERS (NAME,EMAIL,PASSWORD,USER_ROLE, ISADMIN) 
+                VALUES ('${name}', '${email}','${password}', '${user_role}',0)`
+    db.run(content, (err) => {
+        if (err) {
+            console.log(err.message)
+            return res.status(401).send
+        }
+        else
+            return res.status(200).send(`Registered Successfully`)
+    })
+})
+
+
